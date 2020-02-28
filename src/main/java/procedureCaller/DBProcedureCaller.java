35fe34fa90f229f895dbf7a70052de183ee6edd6
PawelTo <pawel.toporowicz@gmail.com>
2020-02-28 02:01:12 +0100

    create oracle database to project

--- src/main/java/procedureCaller/DBProcedureCaller.java
+++ src/main/java/procedureCaller/DBProcedureCaller.java
@@ -1,4 +1,12 @@
 package procedureCaller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DBProcedureCaller {
+
+    @Autowired
+    private SimpleJdbcCall simpleJdbcCall;
 }
