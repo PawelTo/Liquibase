2b3242bf931747b6877fa6e09b418af0a2f25365
PawelTo <pawel.toporowicz@gmail.com>
2020-03-12 01:27:36 +0100

    create jpa and jdbc layer for extendentEntity

--- src/main/java/controller/JdbcJPAController.java
+++ src/main/java/controller/JdbcJPAController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import service.BasicEntity;
-import service.BasicEntityService;
-import service.LiquiBaseEntity;
-import service.LiquiBaseEntityJDBC;
+import service.*;
 
 import java.util.List;
 
@@ -19,6 +16,9 @@ public class JdbcJPAController {
 
     @Autowired
     private BasicEntityService basicEntityService;
+
+    @Autowired
+    private ExtendentEntityService extendentEntityService;
 
     @GetMapping("/getAllLiquibaseJdbc")
     public List<LiquiBaseEntity> getAllLiquiBaseJdbc(){
@@ -44,4 +44,20 @@ public class JdbcJPAController {
     public int addBasicEntityJdbc(String str){
         return basicEntityService.insertJdbc(str);
     }
+
+    @GetMapping("/getAllExtendedByJPA")
+    public List<ExtendentEntity> getAllExtendedByJPA(){return extendentEntityService.getAllJPA();}
+
+    @PostMapping("/addExtendedJPA")
+    public ExtendentEntity addExtendedJPA(String str){return extendentEntityService.addExtendentJPA(str);}
+
+    @GetMapping("/getAllExtendedJdbc")
+    public List<ExtendentEntity> getAllExtendedJdbc(){
+
+        List<ExtendentEntity> allJdbc = extendentEntityService.getAllJdbc();
+        allJdbc.stream().forEach((e)-> System.out.println(e));
+        return allJdbc;}
+
+    @PostMapping("/addExtendedJdbc")
+    public int addExtendedJdbc(String str){ return extendentEntityService.addExtendentJdbc(str);}
 }
--- src/main/java/service/ExtendentEntity.java
+++ src/main/java/service/ExtendentEntity.java
@@ -6,7 +6,18 @@ import javax.persistence.Entity;
 public class ExtendentEntity extends BasicEntity{
     private String onlyExtendentColumn;
 
-    public ExtendentEntity(String stringColumn, int intColumn, Integer integerColumn, long longColumn, double doubleColumn, boolean booleanColumn) {
+    public ExtendentEntity(String stringColumn, int intColumn, Integer integerColumn, long longColumn, double doubleColumn, boolean booleanColumn, String onlyExtendentColumn) {
         super(stringColumn, intColumn, integerColumn, longColumn, doubleColumn, booleanColumn);
+        this.onlyExtendentColumn = onlyExtendentColumn;
+    }
+
+    public ExtendentEntity() {}
+
+    public String getOnlyExtendentColumn() {
+        return onlyExtendentColumn;
+    }
+
+    public void setOnlyExtendentColumn(String onlyExtendentColumn) {
+        this.onlyExtendentColumn = onlyExtendentColumn;
     }
 }
