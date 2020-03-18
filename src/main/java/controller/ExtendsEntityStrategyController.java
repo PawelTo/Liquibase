38f6d17c4a25327cb66be31cba1218e1266e954a
PawelTo <pawel.toporowicz@gmail.com>
2020-03-19 00:04:18 +0100

    preparing single table strategy

--- src/main/java/controller/ExtendsEntityStrategyController.java
+++ src/main/java/controller/ExtendsEntityStrategyController.java
@@ -1,7 +1,6 @@
 package controller;
 
-import extendsEntity.ExtendsService;
-import extendsEntity.MapSubSubEntity;
+import extendsEntity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,27 @@ public class ExtendsEntityStrategyController {
     private ExtendsService extendsService;
 
     @PostMapping("/saveMapSup")
-    public MapSubSubEntity saveMapSup(String str, int id){
-        return extendsService.addJpa(str, id);
+    public MapSubSubEntity saveMapSup(String str, int id) {
+        return extendsService.addMapSubSubJpa(str, id);
+    }
+
+    @PostMapping("/addMapSubEntity")
+    public MapSubEntity addMapSubEntity(String name, int id) {
+        return extendsService.addMapSubJpa(name, id);
+    }
+
+    @PostMapping("/addBaseSingleBaseRepo")
+    public BaseSingeTable addBaseSingleBaseRepo(String name, int id) {
+        return extendsService.addBaseSingleBaseRepo(name, id);
+    }
+
+    @PostMapping("/addExSingleBaseRepo")
+    public ExtSingleTable addExSingleBaseRepo(String name, int id) {
+        return extendsService.addExSingleBaseRepo(name, id);
+    }
+
+    @PostMapping("/addExSingleExRepo")
+    public ExtSingleTable addExSingleExRepo(String name, int id) {
+        return extendsService.addExSingleExRepo(name, id);
     }
 }
--- src/main/java/extendsEntity/ExtendsService.java
+++ src/main/java/extendsEntity/ExtendsService.java
@@ -9,11 +9,50 @@ public class ExtendsService {
     @Autowired
     private MapSubSubRepo mapSubSubRepo;
 
-    public MapSubSubEntity addJpa(String str, int id){
+    @Autowired
+    private MapSubRepo mapSubRepo;
+
+    @Autowired
+    private BaseSingleRepo baseSingleRepo;
+
+    @Autowired
+    private ExSingRepo exSingRepo;
+
+    public MapSubSubEntity addMapSubSubJpa(String str, int id){
         //MapSubSubEntity mapSubSubEntity = new MapSubSubEntity(1+id, "JPA_MapSubSub",str);
         MapSubSubEntity mapSubSubEntity = new MapSubSubEntity();
         mapSubSubEntity.setId(id);
-        mapSubSubEntity.setName("name_"+str);
+        mapSubSubEntity.setName("MapSubSubEntity_"+str);
         return mapSubSubRepo.save(mapSubSubEntity);
+    }
+
+    public MapSubEntity addMapSubJpa(String name, int id){
+        MapSubEntity mapSubEntity = new MapSubEntity();
+        mapSubEntity.setId(id);
+        mapSubEntity.setName("MapSubEntity_"+name);
+        return mapSubRepo.save(mapSubEntity);
+    }
+
+    public BaseSingeTable addBaseSingleBaseRepo(String name, int id){
+        BaseSingeTable baseSingeTable = new BaseSingeTable();
+        baseSingeTable.setId(id);
+        baseSingeTable.setName("addBaseSingleBaseRepo_"+name);
+        return baseSingleRepo.save(baseSingeTable);
+    }
+
+    public ExtSingleTable addExSingleBaseRepo(String name, int id){
+        ExtSingleTable extSingleTable = new ExtSingleTable();
+        extSingleTable.setId(id);
+        extSingleTable.setName("addExSingleBaseRepo_"+name);
+        extSingleTable.setNamesub("addExSingleBaseRepo_SubName_"+name);
+        return baseSingleRepo.save(extSingleTable);
+    }
+
+    public ExtSingleTable addExSingleExRepo(String name, int id){
+        ExtSingleTable extSingleTable = new ExtSingleTable();
+        extSingleTable.setId(id);
+        extSingleTable.setName("addExSingleExRepo_Name_"+name);
+        extSingleTable.setNamesub("addExSingleBaseRepo_SubName_"+name);
+        return exSingRepo.save(extSingleTable);
     }
 }
