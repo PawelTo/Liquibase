7336d2f7a2ee9739b96572be5287f8e2ab30206f
PawelTo <pawel.toporowicz@gmail.com>
2020-03-28 21:58:08 +0100

    add Validated annotation to support validation on method param

--- src/main/java/controller/ValidatedController.java
+++ src/main/java/controller/ValidatedController.java
@@ -3,13 +3,18 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import validation.FirstStepValidation;
 import validation.SecondStepValidation;
 import validation.ValidOb;
 import validation.ValidService;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
+
 @RestController
+@Validated
 public class ValidatedController {
 
     @Autowired
@@ -18,7 +23,7 @@ public class ValidatedController {
     @PostMapping("/addValidated")
     public ValidOb addValidated(@Validated ValidOb valid){
         //Will throw only default exception without group
-        System.out.println("wykonałem metodę Unvalid na obiekcie: "+valid);
+        System.out.println("wykonałem metodę validated na obiekcie: "+valid);
         return validService.addEntityValid(valid);
     }
 
@@ -31,13 +36,27 @@ public class ValidatedController {
 
     @PostMapping("/addFirstLvlValidated")
     public ValidOb addFirstLvlValidated(@Validated(FirstStepValidation.class) ValidOb valid){
-        System.out.println("wykonałem metodę Unvalid na obiekcie: "+valid);
+        System.out.println("wykonałem metodę First level validated na obiekcie: "+valid);
         return validService.addEntityValid(valid);
     }
 
     @PostMapping("/addSecondLvlValidated")
     public ValidOb addSecondLvlValidated(@Validated(SecondStepValidation.class) ValidOb valid){
-        System.out.println("wykonałem metodę Unvalid na obiekcie: "+valid);
+        System.out.println("wykonałem metodę Second level validated na obiekcie: "+valid);
         return validService.addEntityValid(valid);
+    }
+
+    @PostMapping("/methodLvlValidation")
+    public String methodLvlValidation(@RequestParam @Size(min =2, max = 6) String nazwa, @RequestParam @Max(5) int id){
+        String x = "wykonuje validacje na poziomie metody dla wartości nazwa: " + nazwa + " id: "+id;
+        System.out.println(x);
+        return x;
+    }
+
+    @PostMapping("/methodwithOutValidation")
+    public String methodwithOutValidation(String nazwa, int id){
+        String x = "nie wykonuje validacje na poziomie metody dla wartości nazwa: " + nazwa + ", id: " + id;
+        System.out.println(x);
+        return x;
     }
 }
