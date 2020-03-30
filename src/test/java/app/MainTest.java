1312d2f4780f62472fa4cb267932b5421e204e20
PawelTo <pawel.toporowicz@gmail.com>
2020-03-30 02:25:42 +0200

    adding first test cases

---
+++ src/test/java/app/MainTest.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest()//annotation to load spring context
+class MainTest {
+
+    @Test
+    public void contextLoad(){}
+}
