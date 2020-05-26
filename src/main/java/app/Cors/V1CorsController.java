7ed7c6f2f797fdcd4ea5d7089533a09172a560bb
PawelTo <pawel.toporowicz@gmail.com>
2020-05-26 23:05:53 +0200

    controller reformat

--- src/main/java/app/Cors/V1CorsController.java
+++ src/main/java/app/Cors/V1CorsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(V1CorsController.API_PAHT)
-@RestController(V1CorsController.API_PAHT)
+@RestController
 public class V1CorsController {
 
     static final String API_PAHT = "/cors-server";
