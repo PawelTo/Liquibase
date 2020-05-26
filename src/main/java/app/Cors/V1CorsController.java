b149febfd0f7d2f248aabb91b7958785cd786345
PawelTo <pawel.toporowicz@gmail.com>
2020-05-27 00:15:50 +0200

    create endpoint for cors responses

--- src/main/java/app/Cors/V1CorsController.java
+++ src/main/java/app/Cors/V1CorsController.java
@@ -1,9 +1,13 @@
 package app.Cors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "CorsController")
+@ApiModel(description = "CorsController for tutorial", value = "CorsController API Model")
 @RequestMapping(V1CorsController.API_PAHT)
 @RestController
 public class V1CorsController {
